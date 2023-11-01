4cb223cbb1ab9748cb063845ed0ec9f82c5a3756
Bhuiyash Kumar <bhuiyashk@infinite.com>
2023-11-01 11:01:49 +0530

    Asp assignment push

---
+++ ASP/Assignments/Assignment_One/Assignment_One/WebForm1.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Assignment_One.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            select an item
+            <br />
+            <br />
+            <asp:DropDownList ID="DropDownList1" runat="server" AutoPostBack="True" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged1" Width="264px">
+                <asp:ListItem>Select</asp:ListItem>
+                <asp:ListItem Value="bugatti.jpg">Bugatti</asp:ListItem>
+                <asp:ListItem Value="Porsche.jpg">Porsche</asp:ListItem>
+                <asp:ListItem Value="rolls royce.jpg">Rolls Royce</asp:ListItem>
+                <asp:ListItem Value="Defender.jpg">Defender</asp:ListItem>
+                <asp:ListItem Value="innova.jpg">Innova</asp:ListItem>
+            </asp:DropDownList>
+        </div>
+        <asp:Image ID="Image1" runat="server" Height="482px" Width="1082px" />
+        <p>
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" style="width: 84px; height: 35px" Text="Cost" />
+       
+            
+        </p>
+        <p>
+            &nbsp;</p>
+        <asp:Label ID="Label1" runat="server" Text="$0"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ ASP/Assignments/Assignment_One/Assignment_One/WebForm1.aspx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Assignment_One
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                Image1.ImageUrl = $"~/images/Automobiles.jpg";
+            }
+
+        }
+
+        protected void DropDownList1_SelectedIndexChanged1(object sender, EventArgs e)
+        {
+            string name = DropDownList1.SelectedValue;
+            Image1.ImageUrl = $"~/images/{name}";
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string name = DropDownList1.SelectedValue;
+
+            switch (name)
+            {
+                case "bugatti.jpg":
+                    Label1.Text = "$1.9 Million";
+
+                    break;
+                case "Porsche.jpg":
+                    Label1.Text = "$1.3 Million";
+
+                    break;
+                case "innova.jpg":
+                    Label1.Text = "$150000";
+
+                    break;
+                case "Defender.jpg":
+                    Label1.Text = "$1.2 Million";
+
+                    break;
+                case "rolls royce.jpg":
+                    Label1.Text = "$1.7 Million";
+
+                    break;
+            }
+               
+        }
+    }
+}
