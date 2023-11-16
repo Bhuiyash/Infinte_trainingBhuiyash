35a3a1a98bbaf242c546f6741c61ab20f2517393
Bhuiyash Kumar <bhuiyashk@infinite.com>
2023-11-16 16:56:24 +0530

    assignment push

---
+++ WEB API/Assignments/WebApplication3/WebApplication3/Controllers/CountryController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class CountryController : ApiController
+    {
+        List<Country> list = new List<Country>()
+        {
+            new Country{countryCapital="Delhi", countryId=1,countryName="India"},
+            new Country{countryCapital="Canberra", countryId=2,countryName="Australia"}
+        };
+        [HttpGet]
+        public IEnumerable<Country> getAll()
+        {
+            return list;
+        }
+        [HttpPost]
+        public IEnumerable<Country> Create(Country country)
+        {
+            list.Add(country);
+            return list;
+        }
+        [HttpPut]
+        public IEnumerable<Country> Update(int id,[FromUri] int cid, string name, string capital)
+        {
+            Country c = new Country { countryId = cid, countryName = name, countryCapital = capital };
+            list[id - 1] = c;
+            return list;
+
+            
+        }
+        [HttpDelete]
+        public IEnumerable<Country> Delete(int id)
+        {
+            list.RemoveAt(id-1);
+
+            return list;
+        }
+
+    }
+}
---
+++ WEB API/Assignments/WebApplication3/WebApplication3/Models/Country.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication3.Models
+{
+    public class Country
+    {
+        public string countryCapital { get; set; }
+        public string countryName { get; set; }
+        public int countryId { get; set; }
+    }
+}
